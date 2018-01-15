0d41ed7f446a99975247bdbbb75ce2accd393db9
David <david@arloesi.com>
2018-01-15 14:05:26 +0000

    VTCCA-1177 Cleanup TestCreateAccountService

--- test/util/TestCreateAccountService.scala
+++ test/util/TestCreateAccountService.scala
@@ -22,21 +22,20 @@ import com.codahale.metrics.MetricRegistry
 import com.kenshoo.play.metrics.Metrics
 import connectors.{AddressConnector, GroupAccountConnector, IndividualAccountConnector}
 import infrastructure.VOABackendWSHttp
-import models.{GroupAccount, IndividualAccountSubmission, IndividualDetails}
-import org.mockito.ArgumentMatchers.{eq => matches, _}
+import models.{IndividualAccountSubmission, IndividualDetails}
+import org.mockito.ArgumentMatchers.{eq => matches}
 import org.mockito.Mockito._
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.mock.MockitoSugar
 import org.scalatest.{FlatSpec, MustMatchers}
-import play.api.libs.json.{JsValue, Json}
-import uk.gov.hmrc.http.{HeaderCarrier, HttpReads, HttpResponse}
+import play.api.libs.json.Json
+import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
 import uk.gov.hmrc.play.config.inject.ServicesConfig
 import uk.gov.hmrc.play.test.WithFakeApplication
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.Await
 import scala.concurrent.duration._
-import scala.xml.XML
 
 object LOG {
   def apply(message:String): Unit = {
@@ -44,59 +43,36 @@ object LOG {
   }
 }
 
-class DisabledMetrics extends Metrics {
-  override def defaultRegistry: MetricRegistry = new MetricRegistry()
-  override def toJson: String = "{}"
-}
-
-case class RegistrationRequest(fullName: String, plainTextPassword: String, affinityGroup: String = "organisation", emailAddress: String, description: Option[String] = None) {
-  def toXml:scala.xml.Elem =
-      <urn:GsoRegisterPrincipalUserIDXmlInput xmlns:urn="urn:GSO-System-Services:external:1.65:GsoRegisterPrincipalUserIDXmlInput">
-        <urn:PortalIdentifier>
-          HMRC_SMS
-        </urn:PortalIdentifier>
-        <urn:CredentialName>
-          {fullName}
-        </urn:CredentialName>
-        <urn:EmailAddress>
-          {emailAddress}
-        </urn:EmailAddress>
-        {description.fold(<urn:Description/>)(desc =>
-          <urn:Description>{desc}</urn:Description>)}
-        <urn:RegistrationCategory>
-          {affinityGroup}
-        </urn:RegistrationCategory>
-        <urn:Password>
-          {plainTextPassword}
-        </urn:Password>
-      </urn:GsoRegisterPrincipalUserIDXmlInput>
-}
-
-case class KnownFact(key:String,value:String)
-
-object KnownFact {
-  implicit val format = Json.format[KnownFact]
-}
-
-case class Enrolment(key: String, identifier: String, state: String)
-
-object Enrolment {
-  implicit val format = Json.format[Enrolment]
-
-  val ACTIVATED = "Activated"
-  val NOT_YET_ACTIVATED = "NotYetActivated"
-}
-
-case class GovernmentGatewayUser(
-  name:String,username:String,password:String,role:String,affinityGroup:String,
-  credentialIdentifier:String,groupIdentifier:String,enrolments:List[Enrolment],
-  allEnrolments:List[Enrolment],knownFacts:List[KnownFact])
-
-object GovernmentGatewayUser {
-  implicit val format = Json.format[GovernmentGatewayUser]
-}
-
 object TestCreateAccountService extends MockitoSugar {
+  class DisabledMetrics extends Metrics {
+    override def defaultRegistry: MetricRegistry = new MetricRegistry()
+    override def toJson: String = "{}"
+  }
+
+  case class KnownFact(key:String, value:String)
+
+  object KnownFact {
+    implicit val format = Json.format[KnownFact]
+  }
+
+  case class Enrolment(key:String, identifier:String, state:String)
+
+  object Enrolment {
+    implicit val format = Json.format[Enrolment]
+
+    val ACTIVATED = "Activated"
+    val NOT_YET_ACTIVATED = "NotYetActivated"
+  }
+
+  case class GovernmentGatewayUser(
+    name:String, username:String, password:String, role:String, affinityGroup:String,
+    credentialIdentifier:String, groupIdentifier:String, enrolments:List[Enrolment],
+    allEnrolments:List[Enrolment], knownFacts:List[KnownFact])
+
+  object GovernmentGatewayUser {
+    implicit val format = Json.format[GovernmentGatewayUser]
+  }
+
   private implicit val hc: HeaderCarrier = HeaderCarrier()
   private implicit val clock:java.time.Clock = Clock.fixed(Instant.now, ZoneId.systemDefault)
 
@@ -105,95 +81,41 @@ object TestCreateAccountService extends MockitoSugar {
   val http = new VOABackendWSHttp(new DisabledMetrics())
   val config = mock[ServicesConfig]
   when(config.baseUrl(matches("external-business-rates-data-platform"))).thenReturn("http://localhost:9536")
-  val addresses = new AddressConnector(http,config)
-  val individuals = new IndividualAccountConnector(addresses,http)
-  val groups = new GroupAccountConnector(http,config)
-
-  def createOrganisationUser(username:String,password:String,organisationId:String): Future[String] = {
-    registerUser(RegistrationRequest(fullName="Test User",plainTextPassword=password,emailAddress=s"$username@mail.com").toXml)
-  }
-
-  def registerUser(xml: scala.xml.Elem)(implicit hc: HeaderCarrier): Future[String] = {
-    callProxy("portal", "GsoRegisterPrincipalUserID", xml.toString())
-  }
-
-  private def callProxy(api: String, action: String, xml: String)(implicit hc: HeaderCarrier): Future[String] = {
-    println(xml)
-    handleRegisterUserResponse(
-      http.POSTString(
-        s"$ggProxyUrl/government-gateway-proxy/api/$api/$action",
-        xml, Seq("Content-Type" -> "application/xml")
-      )(implicitly[HttpReads[HttpResponse]], hc, implicitly[ExecutionContext])
-    )
-  }
-
-  private def handleRegisterUserResponse(httpResponse: Future[HttpResponse])(implicit ec: ExecutionContext): Future[String] = {
-    httpResponse.map { response =>
-      val responseBodyAsXML = XML.loadString(response.body)
-      (responseBodyAsXML \ "UserID").text
-    }
-  }
+  val addresses = new AddressConnector(http, config)
+  val individuals = new IndividualAccountConnector(addresses, http)
+  val groups = new GroupAccountConnector(http, config)
 
   def createUser(
-    username:String="user1", password:String="pass1",
-    firstName:String="Test", lastName:String="User",
-    email:String="test.user@mail.com", groupId:String="stub-group-3",
-    externalId:String="EXT-1", trustId:String="TRUST-1") = {
+    username:String, password:String, groupId:String, credential:String,
+    firstName:String="Test", lastName:String="User", email:String="test.user@mail.com") = {
 
-    groups.findByGGID(groupId).flatMap(_.fold(Future.successful(())) { group =>
-      val account = IndividualAccountSubmission(externalId=externalId,trustId=trustId,organisationId=group.id,
+    def groupNotFound = throw new Exception(s"Group '$groupId' could not be found")
+    groups.findByGGID(groupId).flatMap(_.fold(groupNotFound) { group =>
+      val account = IndividualAccountSubmission(externalId=credential, trustId="TRUST", organisationId=group.id,
         IndividualDetails(firstName=firstName, lastName=lastName, email=email, phone1=group.phone, phone2=None, addressId=group.addressId))
-      individuals.create(account)
-        .map(_.id).map { accountId =>
-          val enrolment = Enrolment(key="HMRC-VOA-CCA",identifier=accountId.toString,state=Enrolment.ACTIVATED)
-          GovernmentGatewayUser(
-            name=s"$firstName $lastName",username=username,password=password,
-            role="User",affinityGroup="Organisation",credentialIdentifier=accountId.toString,
-            groupIdentifier=groupId,enrolments=List(enrolment),allEnrolments=List(enrolment),
-            knownFacts=List(KnownFact(key="VOAPersonID",value=accountId.toString),KnownFact(key="BusPostcode",value="ABC 123"))) }
-        .flatMap(http.POST[GovernmentGatewayUser,HttpResponse](s"$ggStubUrl/test-only/users",_,Seq("Content-Type"->"application/json")))
-        .map(x => println(s"result: $x"))
+      individuals.create(account).map(_.id).map { accountId =>
+        val enrolment = Enrolment(key="HMRC-VOA-CCA", identifier=accountId.toString, state=Enrolment.ACTIVATED)
+        GovernmentGatewayUser(
+          name=s"$firstName $lastName", username=username,password=password,
+          role="User", affinityGroup="Organisation", credentialIdentifier=credential,
+          groupIdentifier=groupId, enrolments=List(enrolment), allEnrolments=List(enrolment),
+          knownFacts=List(KnownFact(key="VOAPersonID", value=accountId.toString), KnownFact(key="BusPostcode",value="ABC 123"))) }
+        .flatMap(http.POST[GovernmentGatewayUser, HttpResponse](s"$ggStubUrl/test-only/users", _, Seq("Content-Type"->"application/json")))
     })
   }
 }
 
-/*
-personId: 398303440
-organisationId: 171684128
-personal address: 1197007448
-business address: 1197007448
-
-ownerId: 426291880
-organisationId: 1000000005
-personal address: -1630451358
-business address: 1000000000
-
-stub-group-3
-personId: 515833925
-organisationId: 5000000003
-personal address: 1197007448
-business address: 1000000000
- */
 
 class TestCreateAccountServiceSpec extends FlatSpec with MustMatchers with MockitoSugar with ScalaFutures with WithFakeApplication {
-  implicit private lazy val hc: HeaderCarrier = HeaderCarrier()
   val service = TestCreateAccountService
-  val organisationId = "123"
-  val user = "new user"
-
-  val http = new VOABackendWSHttp(new DisabledMetrics())
-  val config = mock[ServicesConfig]
-  when(config.baseUrl(matches("external-business-rates-data-platform"))).thenReturn("http://localhost:9536")
-  val addresses = new AddressConnector(http,config)
-  val individuals = new IndividualAccountConnector(addresses,http)
 
   it should "Create user in Data Platform and GG stubs" in {
-    val result = service.createUser(username="user3",password="pass3",groupId="stub-group-3")
-    Await.result(result,10 seconds)
+    val id = 5
+    val result = service.createUser(username=s"user$id", password=s"pass$id", groupId="stub-group-3", credential=s"cred$id")
+    Await.result(result, 10 seconds)
   }
 
   "GG user service" should "create user under a GG organisation" in {
-    // Await.result(TestCreateAccountService.createOrganisationUser("username1","password1","org1"),10 seconds)
   }
 
   "created user" should "not be asked to register when logging in" in {
